9ce33b21194412c8eaa48832975c2c5ad1840d8b
Jessietbl <46272879+Jessietbl@users.noreply.github.com>
2026-07-05 18:10:39 +0800

    Add CSVReader class for reading CSV files

--- include/csv_reader.h
+++ include/csv_reader.h
@@ -1,1 +1,12 @@
+#pragma once
 
+#include <vector>
+#include <string>
+
+#include "process_data.h"
+
+class CSVReader
+{
+public:
+    std::vector<ProcessData> read(const std::string& filename);
+};
